0979737f711ef7a4a63581a05c12c21160d619b6
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-01 16:40:35 +0300

    My first commit!

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,19 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int intC = 99;
+        int intH = 104;
+        int intA = 97;
+        int intR = 114;
+
+        char c = (char) intC;
+        char h = (char) intH;
+        char a = (char) intA;
+        char r = (char) intR;
+
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
